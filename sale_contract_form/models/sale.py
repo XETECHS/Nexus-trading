e4358457d09ea7ad33129bc9e68b0e8365a262f4
Abraham Febres <afebres@xetechs.com>
2021-02-24 18:10:34 -0400

    new module: sale_contract

---
+++ sale_contract_form/models/sale.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    # Sale Contrat Details
+
+    country_origin_id = fields.Many2one('res.country', string='Origin', required=True)
+    crop_year_id = fields.Many2one('sale.contract.crop.year', string='Crop Year', required=True) 
+    quaily_specs = fields.Text(string='Quality/Specs', required=True)
+    packing_id = fields.Many2one('sale.contract.packing', string='Packing', required=True)
+    details = fields.Text(string='Details', required=True)
+
+    shipment_date = fields.Date(string='Shipment Date')
+    shipment_date_end = fields.Date(string='Shipment Date End')
+
+    loading_port_id = fields.Many2one('sale.contract.port', string='Loading Port', required=True)
+    destination_port_id = fields.Many2one('sale.contract.port', string='Destination Port', required=True)
+
+    document_ids = fields.Many2many('sale.contract.document', string='Document')
+
+    remarks_id = fields.Many2one('sale.contract.remark', string='Remarks')
+    remarks = fields.Text(string='Remarks Text')
+
+    payment_term2_id = fields.Many2one(
+        'account.payment.term', string='Payment Terms2', check_company=True,  # Unrequired company
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",)
+
+    payment_term_text = fields.Text(string='Payment Term Text')
+
+    
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    containers = fields.Float(string='Container', digits=(6, 2))
+    con_dimension = fields.Many2one('comodel_name', string='Containers Dimesion')
---
+++ sale_contract_form/models/sale_contract.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+
+
+class SaleContractCropYear(models.Model):
+    _name = 'sale.contract.crop.year'
+    _description = 'Sale Contract Crop Year'
+
+    name = fields.Char(string='Name', required=True)
+    data = fields.Char(string='Data')
+    data2 = fields.Char(string='Data2')
+
+
+class SaleContractPacking(models.Model):
+    _name = 'sale.contract.packing'
+    _description = 'Sale Contract Packing'
+
+    name = fields.Char(string='Name', required=True)
+    qty = fields.Float(string='Qty', digits=(6, 2), required=True)
+    uom_id = fields.Many2one('uom.uom', string='UoM', required=True)
+    comment = fields.Text(string='Comment')
+
+
+class SaleContractPort(models.Model):
+    _name = 'sale.contract.port'
+    _description = 'Sale Contract Port'
+
+    name = fields.Char(string='Name', required=True)
+    city = fields.Char(string='City')
+    country_id = fields.Many2one('res.country', string='Origin', required=True)
+
+
+class SaleContractDocument(models.Model):
+    _name = 'sale.contract.document'
+    _description = 'Sale Contract Document'
+
+    name = fields.Char(string='Name')
+    data = fields.Char(string='Data')
+    data2 = fields.Char(string='Data2')
+    comment = fields.Text(string='Comment')
+
+
+class SaleContractRemark(models.Model):
+    _name = 'sale.contract.remark'
+    _description = 'Sale Contract Remark'
+
+    name = fields.Char(string='Name')
+    data = fields.Char(string='Data')
+    data2 = fields.Char(string='Data2')
+
+class SaleContractDimension(models.Model):
+    _name = 'sale.contract.dimension'
+    _description = 'Sale Contract Dimension'
+
+    name = fields.Char(string='Name')
+    data = fields.Char(string='Data')
+    data2 = fields.Char(string='Data2')
